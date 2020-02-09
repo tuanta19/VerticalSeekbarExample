d81c94504694ef4df9b188b6e523a8f88f0995ef
tuanta19 <techman2020@gmail.com>
2020-02-09 16:24:49 +0700

    Initial commit

---
+++ app/src/main/java/com/kerrystudio/verticalseekbar_example/MainActivity.java
@@ -0,0 +1,13 @@
+package com.kerrystudio.verticalseekbar_example;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
